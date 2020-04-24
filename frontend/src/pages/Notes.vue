273f776414955d62a6dbb80e64dd55f848d6380b
Asha <asha.vakkattil@gmail.com>
2020-04-24 18:09:09 +0530

    Added notes page (dummy)

---
+++ frontend/src/pages/Notes.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page>
+    <main-header :pageTitle="title" :tabColour="tabColour" />
+    <div>Notes page</div>
+  </q-page>
+</template>
+<script>
+import MainHeader from "../components/MainHeader";
+export default {
+  components: { MainHeader },
+  data() {
+    return {
+      title: "Notes",
+      tabColour: 'warning'
+    };
+  }
+};
+</script>
